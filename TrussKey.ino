3b2c91df8821a9655f3d14aaa5cd2d89343d2778
unknown <Charlie Xu>
2017-12-21 18:33:45 -0500

    BreaV5; Software includes i2c battery

--- TrussKey.ino
+++ TrussKey.ino
@@ -3,12 +3,14 @@
 #include <SoftwareSerial.h>
 #include <avr/sleep.h>
 #include <avr/wdt.h>
-#include "SIM800.h"
+//#include "SIM800.h"
+#include "MAX17043.h"
+#include "Wire.h"
+
 
 #define APN "connect"
 #define WARMUP 300
 #define con Serial
-
 
 int mq3_analogPin = A2; // connected to the output pin of MQ3 
 int solenoid_Pin = 10;
@@ -36,9 +38,9 @@ int longPressActive = 0;
 
 /*GPRS module variables*/
 
-char* smsMSG = "TrussKey is being tampered\032";
-char* smsNum = "+14125195973";
-CGPRS_SIM800 gprs;
+//char* smsMSG = "TrussKey is being tampered\032";
+//char* smsNum = "+14125195973";
+//CGPRS_SIM800 gprs;
 uint32_t count = 0;
 uint32_t errors = 0;
 int switchPin = A1;
@@ -48,19 +50,25 @@ unsigned long time;
 
 volatile byte state = LOW;
 
+MAX17043 batteryMonitor;
+
+
 void setup(){
+  Wire.begin();
   con.begin(9600); // open serial at 9600 bps
   pinMode(mq3_analogPin,INPUT);
   pinMode(solenoid_Pin,OUTPUT);
   pinMode(BlueLED,OUTPUT);
   pinMode(GreenLED,OUTPUT);
   pinMode(RedLED,OUTPUT);
+  pinMode(4,OUTPUT);
 
   pinMode(pwrPin,INPUT);
-  attachInterrupt(digitalPinToInterrupt(detSW_Pin),det_SW,HIGH);
-
+//  attachInterrupt(digitalPinToInterrupt(detSW_Pin),det_SW,HIGH);
+  batteryMonitor.reset();
+  batteryMonitor.quickStart();
   //turnLEDOff();
-  con.println("Start");
+  //con.println("Start");
 }
 
 void loop()
@@ -68,8 +76,8 @@ void loop()
   turnLEDOff();
   if (digitalRead(pwrPin)){
     readingCounter++;
-    con.print("Reading counter");
-    con.println(readingCounter);
+    //con.print("Reading counter");
+    //con.println(readingCounter);
 
     /*powerOn determines whether power pin gets pressed
       Keep looping till the pin is set high
@@ -85,6 +93,7 @@ void loop()
         turnBlueOn();
         powerOn = 1;
       }
+      /*Clear warm up timer for sensor*/
       else{
         //readingCounter = 0;
         turnLEDOff();
@@ -92,19 +101,38 @@ void loop()
         timer = 0;
       }
     }
+    /*Clear warm up timer for sensor*/
     else{
       powerOn = 0;
       //readingCounter++;
-      con.println("Turn Off");
+      //con.println("Turn Off");
       turnLEDOff();
       timer = 0;
     }
     delay(500);
   }
-  //powerOn = 1;
+  powerOn = 1;
 
   /*After power is on*/
   if (powerOn){
+    delay(1000);
+    float cellVoltage = batteryMonitor.getVCell();
+    //con.print("Voltage:\t\t");
+    //con.print(cellVoltage, 4);
+    //con.println("V");
+  
+    float stateOfCharge = batteryMonitor.getSoC();
+    if (stateOfCharge < 80){
+      digitalWrite(4,HIGH);
+      con.println("Too low");
+    }
+    else{
+      digitalWrite(4,LOW);
+      con.println("High enough");
+    }
+    //con.print("State of charge:\t");
+    //con.print(stateOfCharge);
+    //con.println("%");
     turnBlueOn();
     digitalWrite(solenoid_Pin,LOW);
     passed = 1;
@@ -129,7 +157,7 @@ void loop()
 
     /*After warm up, light up LED to cue user to breathe*/
     if (timer > WARMUP){
-      con.println("Start");
+      //con.println("Start");
       //digitalWrite(led2,LOW);
       for (int i = 0; i < 5;i++){
         turnBlueOn();
@@ -147,21 +175,21 @@ void loop()
       for (int i = 0; i < 15; i++){
         int mq3_value = 1023 - analogRead(mq3_analogPin);
         values[i] = mq3_value;
-        con.print("Testing val is");
-        con.println(values[i]);
+        //con.print("Testing val is");
+        //con.println(values[i]);
         if (values[i] > max_val){
           max_val = values[i];
         }
         if (i > -1){
           diff = values[i] - base_val;
-          con.print("diff=");
-          con.println(diff);
+          //con.print("diff=");
+          //con.println(diff);
           if (diff > 160){
             turnRedOn();
             digitalWrite(solenoid_Pin,LOW);
             delay(3000);
             passed = 0;
-            con.println("FAIL");
+            //con.println("FAIL");
             break;
           }
         }
@@ -169,10 +197,14 @@ void loop()
       }
       if ((passed == 1) && ((max_val - base_val) > 20)){ 
         turnGreenOn();
+        delay(500);
         digitalWrite(solenoid_Pin,HIGH);
+        passed = 2;
       }
-      //turnRedOn();
-      turnGreenOn();
+      if (passed != 2){
+        turnRedOn();
+        delay(500);
+      }
       delay(1000);
       /*Resets all values*/
       for (int i = 0; i < 15; i++){
@@ -182,7 +214,7 @@ void loop()
       base_val = 0;
       timer = 0;
       turnLEDOff();
-      delay(3000);
+      delay(2000);
     }
   }
 }
@@ -213,29 +245,29 @@ void turnGreenOn()
 }
 
 
-void det_SW()
-{
-  /*Initiate GPRS*/
-  interrupts();
-  
-  for (;;) {
-    while (!gprs.init()) {
-    }
-    byte ret = gprs.setup(APN);
-    if (ret == 0)
-      break;
-  }
-
-  for (;;) {
-    if (gprs.httpInit()) break;
-    //con.println(gprs.buffer);
-    gprs.httpUninit();
-    delay(1000);
-  }
-
-  smsSent = gprs.sendSMS(smsNum,smsMSG);
-
-  noInterrupts();
-  //state = !state;
-  //con.print("Det Tampered\n");
-}
+//void det_SW()
+//{
+//  /*Initiate GPRS*/
+//  interrupts();
+//  
+//  for (;;) {
+//    while (!gprs.init()) {
+//    }
+//    byte ret = gprs.setup(APN);
+//    if (ret == 0)
+//      break;
+//  }
+//
+//  for (;;) {
+//    if (gprs.httpInit()) break;
+//    //con.println(gprs.buffer);
+//    gprs.httpUninit();
+//    delay(1000);
+//  }
+//
+//  smsSent = gprs.sendSMS(smsNum,smsMSG);
+//
+//  noInterrupts();
+//  //state = !state;
+//  //con.print("Det Tampered\n");
+//}
